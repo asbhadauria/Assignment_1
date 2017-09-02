13ac5c59b03439eb9af49fdb27d15f506041ea63
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-03 03:15:33 +0530

    Add files via upload

---
+++ A1_Q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;float num1;double num2;char c;
+	cout<<"Enter an integer literal"<<"\n";
+	cin>>num;
+	cout<<"Enter an floating point literal"<<"\n";
+	cin>>num1;
+	cout<<"Enter an double literal"<<"\n";
+	cin>>num2;
+	cout<<"Enter an Character"<<"\n";
+	cin>>c;
+	cout<<"Entered integer literal"<<num<<"\n";
+	cout<<"Entered floating point literal"<<num1<<"\n";
+	cout<<"Entered double literal"<<num2<<"\n";
+	cout<<"Entered character literal"<<c;
+	return 0;
+}
