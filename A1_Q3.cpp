2eab2c5a174ab2630006dd6e3dd95cdadbcd72ab
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 14:45:32 +0530

    Add files via upload

---
+++ A1_Q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int g1,g2;
+cout<< "Enter 2 Integers:";
+cin>>g1>>g2;
+cout<< "Sum:"<<g1+g2<<"\n";
+cout<< "Product:"<<g1*g2<<"\n";
+cout<< "Difference:"<<g1-g2<<"\n";
+cout<< "Quotient:"<<g1/g2<<"\n";
+cout<< "Remainder:"<<g1%g2<<"\n";
+return 0;
+}
